c14cde7e78a79bd48edc70b5212d1c91875a95f9
Daniel Stelea <daniel@digitnord.no>
2023-12-23 22:24:02 +0200

    Fix fireball rotation

--- scenes/PlayerSpells/TwistedFireball/TwistedFireball.gd
+++ scenes/PlayerSpells/TwistedFireball/TwistedFireball.gd
@@ -1,7 +1,7 @@
 extends Sprite2D
 @export var speed: float = 200.0
 var distance_from_parent: float = 0.0
-var degress = randf_range(0.0, 360.0)
+var degrees = randf_range(0.0, 360.0)
 
 
 # Called when the node enters the scene tree for the first time.
@@ -12,6 +12,6 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	# rotate the sprite around the center of the parent node
-	rotation_degrees += degress * speed
-	position.y = sin(rotation_degrees / 180.0 * PI) * distance_from_parent
-	position.x = cos(rotation_degrees / 180.0 * PI) * distance_from_parent
+	rotation_degrees += delta * speed
+	position.y = sin(rotation_degrees / degrees * PI) * distance_from_parent
+	position.x = cos(rotation_degrees / degrees * PI) * distance_from_parent
